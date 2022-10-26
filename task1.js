85c6d4d3a78470cedfd74e8264106f81c3dc9f90
ValentynaBerezhniak <vberezhnyak1998@gmail.com>
2022-10-27 00:43:21 +0300

    first PR

---
+++ task1.js
@@ -0,0 +1,7 @@
+let str1 = prompt("Word");
+let str2 = prompt("Word");
+let str3 = prompt("Word");
+
+const strNumber = `${str1} ${str2} ${str3}`
+
+console.log(strNumber)
---
+++ task2.js
@@ -0,0 +1,4 @@
+const row = '43567'
+const colum = `${row[0]} ${row[1]} ${row[2]} ${row[3]} ${row[4]}`
+
+console.log(colum)
