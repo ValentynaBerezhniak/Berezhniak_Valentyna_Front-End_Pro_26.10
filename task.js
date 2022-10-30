b20ade265a2f26cf36cd931a6e718ab4f34fa399
ValentynaBerezhniak <vberezhnyak1998@gmail.com>
2022-10-30 23:04:17 +0300

    answer

--- task.js
+++ task.js
@@ -2,6 +2,12 @@ document.addEventListner
 const number1 = Number(prompt('First number'));
 const number2 = Number(prompt('Second number'));
 
-const answer = (number1 + number2)
+const answerplus = (number1 + number2)
+const answerminus = (number1 - number2)
+const answermultiply = (number1 * number2)
+const answerdivide = (number1 / number2)
 
-alert = console.log(answer)
+alert (`${(number1+" + "+number2+" = "+ answerplus)} 
+${(number1+" - "+number2+" = "+ answerminus)} 
+${(number1+" * "+number2+" = "+ answermultiply)} 
+${(number1+" / "+number2+" = "+ answerdivide)}`)
