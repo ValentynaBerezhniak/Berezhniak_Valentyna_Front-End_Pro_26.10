e7d4fae720516724b89fc90f149c07ab17e2ab15
ValentynaBerezhniak <vberezhnyak1998@gmail.com>
2022-10-30 21:50:29 +0300

    task1

---
+++ task.js
@@ -0,0 +1,7 @@
+document.addEventListner
+const number1 = Number(prompt('First number'));
+const number2 = Number(prompt('Second number'));
+
+const answer = (number1 + number2)
+
+alert = console.log(answer) 
