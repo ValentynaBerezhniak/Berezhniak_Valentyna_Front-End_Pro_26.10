ab0b121e8bb6a3ba8724bc299a5b57ac783e71e1
ValentynaBerezhniak <vberezhnyak1998@gmail.com>
2022-10-31 18:52:51 +0300

    answer

--- task.js
+++ task.js
@@ -5,4 +5,4 @@ const number3 = Number(prompt('Please enter the third number'))
 const sum = number1 + number2 + number3
 const arithmeticMean =  sum / 3
 
-alert("Arithmetic mean : "+arithmeticMean )
+alert("Arithmetic mean : " + arithmeticMean )
