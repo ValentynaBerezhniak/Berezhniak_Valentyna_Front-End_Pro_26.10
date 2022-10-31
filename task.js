a0ca6fd118f07456ad4897d9a95290b6f829a1a8
ValentynaBerezhniak <vberezhnyak1998@gmail.com>
2022-10-31 18:52:09 +0300

    answer

---
+++ task.js
@@ -0,0 +1,8 @@
+document.addEventListner
+const number1 = Number(prompt('Please enter the first number'))
+const number2 = Number(prompt('Please enter the second number'))
+const number3 = Number(prompt('Please enter the third number'))
+const sum = number1 + number2 + number3
+const arithmeticMean =  sum / 3
+
+alert("Arithmetic mean : "+arithmeticMean )
