9ec8f0f35b09148104edf7e566a8bfd8bd49becf
ValentynaBerezhniak <vberezhnyak1998@gmail.com>
2022-11-07 22:11:59 +0300

    answer

--- task.js
+++ task.js
@@ -1,67 +1,20 @@
-const usersBirthdayYear = prompt('Уведіть Ваш рік народження')
-const currentYear = new Date(); 
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-if (usersBirthdayYear !== null && usersBirthdayYear !== '') {
-    let date = new Date();  
-    let userYear = `Тобі ${date.getFullYear() - usersBirthdayYear}`; 
+switch(numOrStr) {
 
-    const usersCity = prompt ('В якому місті Ви живете?')
+    case null : 
+        console.log('ви скасували');
+        break;
 
-    const capitals = ['Київ' , 'Вашингтон' , 'Лондон']
-    const countries = ['України' , 'США' , 'Великої Британії']
-    
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
 
-    if (usersCity !== null && usersCity !== ''){
-        if (usersCity === capitals[0]) {
-            let userCapitals = `Ти живеш у столиці ${countries[0]} `   // 'Ти живеш у столиці ' + countries[0];
-            alert( `${userYear} 
-${userCapitals}`)
-        } 
-        else if (usersCity === capitals[1]) {
-            let userCapitals = `Ти живеш у столиці ${countries[1]} `;
-            alert( `${userYear} 
-${userCapitals}`)
-        }
-         else if (usersCity === capitals[2]) {
-            let userCapitals = `Ти живеш у столиці ${countries[2]} `;
-            alert( `${userYear} 
-${userCapitals}`)
-        } 
-        else {
-            let userCapitals = `Ти живеш у місті ${usersCity} ` //'Ти живеш у місті ' + usersCity;
-            alert( `${userYear} 
-${userCapitals}`)
-        }
+    case (isNaN( +numOrStr )): 
+        console.log(' number is Ba_NaN');
+        break;
 
-        const favoriteSport= prompt ('Який Ваш улюблений вид спорту?')
-        const sport = ['Бокс' , 'Баскетбол' , 'Футбол']
-        const sportsman = ['Майком Тайсоном' , 'Майклом Джорданом' , 'Андрєм Шевченком']
-
-        if (favoriteSport !== null && favoriteSport !== ''){
-            if (favoriteSport === sport[0]) {
-                let userSport = 'Круто! Хочеш стати ' + sportsman[0];
-                alert( userSport)
-            } 
-            else if (favoriteSport === sport[1]) {
-                let userSport = 'Круто! Хочеш стати  ' + sportsman[1];
-                alert( userSport)
-            }
-             else if (favoriteSport === sport[2]) {
-                let userSport = 'Круто! Хочеш стати  ' + sportsman[2];
-                alert( userSport)
-            } 
-            else {
-                let userSport = 'Круто! Твій улюблений спорт  ' + favoriteSport;
-                alert( userSport)
-            }
-        } 
-        else {
-            alert('Шкода, що Ви не захотіли ввести свій улюблений спорт')
-        }
-    } else {
-        alert('Шкода, що Ви не захотіли ввести своє місто')
-    } 
-
-} else {
-    alert('Шкода, що Ви не захотіли ввести свій вік')
+    default:
+        console.log('OK!')
 }
